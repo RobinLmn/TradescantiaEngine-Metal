a3ac7e8a432cc4a51d47c6011b083093da923d77
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2022-01-15 16:39:57 -0500

    Project Initialization

---
+++ TradescantiaEngine/TradescantiaEngine/Misc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TradescantiaEngine
+//
+//  Created by Robin Leman on 2022-01-15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
